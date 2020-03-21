a0f150e7d029e62024002c7697490e2b760d9457
d.markov <agushasok@mail.ru>
2020-03-21 23:50:16 +0300

    fix users list

--- src/app/modules/top/top.module.ts
+++ src/app/modules/top/top.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TopRoutingModule } from './top-routing.module';
 import { ViewComponent } from './components/view/view.component';
+import { UserComponent } from './components/user/user.component';
+import { ListComponent } from './components/list/list.component';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    SharedModule,
     TopRoutingModule
   ],
-  declarations: [ViewComponent]
+  declarations: [
+    ViewComponent,
+    UserComponent,
+    ListComponent
+  ]
 })
 export class TopModule { }
