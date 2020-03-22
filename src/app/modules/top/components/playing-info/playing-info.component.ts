20d2b17504a09bcafb6ce20c746e4905ceed08f8
d.markov <agushasok@mail.ru>
2020-03-22 13:13:48 +0300

    add songs component

---
+++ src/app/modules/top/components/playing-info/playing-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-playing-info',
+  templateUrl: './playing-info.component.html',
+  styleUrls: ['./playing-info.component.scss']
+})
+export class PlayingInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
